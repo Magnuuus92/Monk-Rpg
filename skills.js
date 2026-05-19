1bfa19701ce5d4e5277d998fb9f7552f79353f82
Magnus <Magnus92.kodehode@gmail.com>
2026-05-19 13:13:32 +0200

    Updated skill tooltips

--- skills.js
+++ skills.js
@@ -17,50 +17,47 @@ const SKILL_DATA = {
   },
   a2: {
     name: "Marvelous",
-    description: "(Passive) descriptionplaceholder",
+    description: "(Passive) Gain small amount of energy per successful hit",
     requiresText: "a1 unlocked",
     requires: () => playerState.skills.a1,
   },
   a3: {
     name: "Spike Adrenaline",
-    description: "(Active) descri placehold",
+    description: "(Active) Sacrifice som Health for a rush of adrenaline",
     requiresText: "a1 unlocked",
     requires: () => playerState.skills.a1,
   },
   a4: {
     name: "Mighty Cranial Strike",
-    description:
-      "(Active) Strikes twice in quick succession. Scales with STR & DEX",
+    description: "(Active) Powerful strike",
     requiresText: "a1 unlocked",
     requires: () => playerState.skills.a1,
   },
   a5: {
-    name: "Fiery Resolve",
-    description:
-      "(Active ability) Strikes twice in quick succession. Scales with STR & DEX",
+    name: "Die Hard",
+    description: "(passive) You regain some health if below 30% HP every turn.",
     requiresText: "any of a2, a3 or a4 unlocked.",
     requires: () =>
       playerState.skills.a2 || playerState.skills.a3 || playerState.skills.a4,
   },
   a6: {
     name: "Force of Nature",
-    description:
-      "(Passive) +4 to all stats and every second time strike is triggered gain +1 flat dmg this combat.",
+    description: "(Passive) +4 to all stats.",
     requiresText: "any of a2, a3 or a4 unlocked.",
     requires: () =>
       playerState.skills.a2 || playerState.skills.a3 || playerState.skills.a4,
   },
   a7: {
-    name: "One Two Combo",
+    name: "Power Smash",
     description:
-      "(Active ability) Strikes twice in quick succession. Scales with STR & DEX",
+      "(Active ability) Very powerful, but at the cost of some of your own health.",
     requiresText: "a5 or a6 unlocked.",
     requires: () => playerState.skills.a5 || playerState.skills.a6,
   },
   // Discipline
   b1: {
     name: "Defensive Strike",
-    description: "(Active)",
+    description: "(Active) Strike once and gain small amount of block.",
     requiresText: "none.",
     requires: () => true,
   },
@@ -78,13 +75,13 @@ const SKILL_DATA = {
   },
   b4: {
     name: "flurry",
-    description: "(Active) Five consecutive hits with lesser power.",
+    description: "(Active) Strike four times.",
     requiresText: "b1 unlocked.",
     requires: () => playerState.skills.b1,
   },
   b5: {
     name: "Body Positive",
-    description: "(Passive) Retaliation now also scales with resilience.",
+    description: "(Passive) Strike now also scales with resilience.",
     requiresText: "b2, b3 or b4 unlocked.",
     requires: () =>
       playerState.skills.b2 || playerState.skills.b3 || playerState.skills.b4,
@@ -99,38 +96,41 @@ const SKILL_DATA = {
   },
   b7: {
     name: "Evasive Maneuver",
-    description: "(Active) placehold",
+    description: "(Active) Gain a decent amount of block.",
     requiresText: "b5 or b6 unlocked.",
     requires: () => playerState.skills.b5 || playerState.skills.b6,
   },
   //spirit
   c1: {
     name: "Pulsating Forcefield",
-    description: "(Active)",
+    description:
+      "(Active) Deal damage to an enemy and you gain block (scales with INT and WIS).",
     requiresText: "none.",
     requires: () => true,
   },
   c2: {
     name: "Focus Mind",
-    description: "(Active) Gain block at the start of every turn.",
+    description:
+      "(Active) Gain some energy now and a little bit at the start of every turn.",
     requiresText: "c1 unlocked.",
     requires: () => playerState.skills.c1,
   },
   c3: {
     name: "Time Dabble",
-    description: "(Active) placehold.",
+    description: "(Active) Gain 2 ap.",
     requiresText: "c1 unlocked.",
     requires: () => playerState.skills.c1,
   },
   c4: {
     name: "Chi Blast",
-    description: "(Active) Gain block at the start of every turn.",
+    description: "(Active) High damage (Scales with LVL, WIS and INT).",
     requiresText: "c1 unlocked.",
     requires: () => playerState.skills.c1,
   },
   c5: {
     name: "Unlock Spirit",
-    description: "(Passive) Empower Focus Mind and Energy Harvest.",
+    description:
+      "(Passive) Empower Focus Mind, Energy Harvest and Spiritual Trickery.",
     requiresText: "c2, c3 or c4 unlocked.",
     requires: () =>
       playerState.skills.c2 || playerState.skills.c3 || playerState.skills.c4,
@@ -145,45 +145,47 @@ const SKILL_DATA = {
   },
   c7: {
     name: "Energy Harvest",
-    description: "(Active) placehold",
+    description:
+      "(Active) Trigger Pulsating Forcefield once and accumulate energy.",
     requiresText: "c5 or c6 unlocked.",
     requires: () => playerState.skills.c5 || playerState.skills.c6,
   },
   // Force - discipline Crossbranch
   ab1: {
     name: "Sweeping Flurry",
-    description: "(Active)",
+    description: "(Active) Strike each enemy 4 times.",
     requiresText: "a7 and b7 unlocked.",
     requires: () => playerState.skills.a7 && playerState.skills.b7,
   },
   ab2: {
     name: "Strength Beyond Measure",
-    description: "(Passive)",
+    description: "(Passive) Gain 10 STR, DEX, VIT and RES.",
     requiresText: "ab1 unlocked.",
     requires: () => playerState.skills.ab1,
   },
   ab3: {
     name: "Retaliation Specialist",
-    description: "(Passive)",
+    description: "(Passive) +15% Retaliation chance.",
     requiresText: "ab1 unlocked.",
     requires: () => playerState.skills.ab1,
   },
   //Discipline - Spirit Cross
   bc1: {
     name: "Spiritual Trickery",
-    description: "(Active)",
+    description: "(Active) Gain block and accumulate energy twice.",
     requiresText: "b7 and c7 unlocked.",
     requires: () => playerState.skills.b7 && playerState.skills.c7,
   },
   bc2: {
     name: "Dual Potency",
-    description: "(Passive)",
+    description:
+      "(Passive) Energyharvest trigger c1 twice. Defensive state of mind retains 70% of remaining block.",
     requiresText: "bc1 unlocked.",
     requires: () => playerState.skills.bc1,
   },
   bc3: {
     name: "Spiritual Awekening",
-    description: "(Passive)",
+    description: "(Passive) Gain 100 MaxHP and 100 MaxEnergy.",
     requiresText: "bc1 unlocked.",
     requires: () => playerState.skills.bc1,
   },
@@ -199,31 +201,33 @@ const SKILL_DATA = {
   },
   r2: {
     name: "Master of Discipline",
-    description: "(Passive)",
+    description:
+      "(Passive) Every second time strike is triggered gain +1 flat dmg this combat.",
     requiresText: "5 unlocked in force + 5 in discipline",
     requires: () => countUnlocked("a") >= 5 && countUnlocked("b") >= 5,
   },
   r3: {
     name: "Limit Break",
-    description: "(Passive)",
+    description:
+      "(Passive) You can surpass MaxHealth and MaxEnergy in combat. (NOT YET IMPLEMENTED)",
     requiresText: "5 unlocked in force + 5 in spirit.",
     requires: () => countUnlocked("a") >= 5 && countUnlocked("c") >= 5,
   },
   r4: {
     name: "Defensive spirits",
-    description: "(Passive)",
+    description: "(Passive) placeholder",
     requiresText: "5 unlocked in discipline + 5 in spirit.",
     requires: () => countUnlocked("b") >= 5 && countUnlocked("c") >= 5,
   },
   r5: {
     name: "Unlocked Pendant",
-    description: "(Passive)",
+    description: "(Passive) placeholder",
     requiresText: "WorldReqPlaceholder.",
     requires: () => false, //placehold
   },
   r6: {
     name: "Divine Knowledge",
-    description: "(Passive)",
+    description: "(Passive) placeholder",
     requiresText: "WorldReqPlaceholder.",
     requires: () => false, //placehold
   },
